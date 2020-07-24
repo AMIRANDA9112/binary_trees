91d257662efb40adc5f517154d8bf0cfee4f1ef6
AMIRANDA9112 <andreselfm@gmail.com>
2020-07-24 18:51:52 -0500

    less conditions

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,28 +1,21 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - verify if a node is a leaf of a tree
+ * binary_tree_is_root - verify if a node is a root of a tree
  * @node: node to analyze
- * Return: 1 if leaf & zero if not y can noy fajarding
+ * Return: 1 if leaf & zero if not y can not fajarding
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if (node == NULL)
-    {
-        return (0);
-    }
-    else if (node->left != NULL && node->right != NULL)
-    {
-        if (node->parent == NULL)
-        {
-            return (1);
-        }
-        else
-        {
-            return (0);
-        }
-    }
-    else
-    {
-        return (0);
-    }
+	if (node == NULL)
+	{
+		return (0);
+	}
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
