16b97176a726a5438aef73c169a87d649c77215f
AMIRANDA9112 <andreselfm@gmail.com>
2020-07-24 19:25:03 -0500

    preorder recursion method

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - funtion that through a binary tree in preorder
+ * @tree: Tree in analyze
+ * @func: printer function
+ * Return: void function
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree == NULL || func == NULL)
+        return;
+    func(tree->n);
+    binary_tree_preorder(tree->left, func);
+    binary_tree_preorder(tree->right, func);
+}
