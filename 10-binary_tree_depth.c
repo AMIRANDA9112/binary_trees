bc660dc665843a9d4afe70cabe6de67743a20e71
AMIRANDA9112 <andreselfm@gmail.com>
2020-07-24 20:47:12 -0500

    nodes count

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,9 +7,9 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
-    if (tree->parent == NULL)
-        return (0);
-    return (1 + binary_tree_depth(tree->parent));
+	if (tree == NULL)
+		return (0);
+	if (tree->parent == NULL)
+		return (0);
+	return (1 + binary_tree_depth(tree->parent));
 }
